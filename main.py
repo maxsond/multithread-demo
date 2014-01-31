28bac0540cbeda4d10d0e611136a98161b00054b
indigochill <vagus.eques@gmail.com>
2014-01-31 16:43:03 -0500

    renamed thread module to 'threadz' to enable importing

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import display
-from thread import myThread
+from threadz import myThread
 		
 thread1 = myThread(1, "Thread-1", display.lwin)
 thread2 = myThread(2, "Thread-2", display.rwin)
 		
 thread1.start()
 thread2.start()
+display.clear(display.scr)
 display.end(display.scr)
 display.curses.endwin()
