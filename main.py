cb6b60e22b56c7129ed01d3512996f2e6963108b
indigochill <vagus.eques@gmail.com>
2014-01-30 20:32:06 -0500

    Broke thread management into a separate module

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-import threading
-import time
 import display
-
-class myThread(threading.Thread):
-
-	def __init__(self,threadID,name,win):
-		threading.Thread.__init__(self)
-		self.threadID = threadID
-		self.name = name
-		self.win = win
-	def run(self):
-		display.refresh()
-		display.msg("ABC DEF GHI JKL MNO PQR STU VWX",False,self.win)
+from thread import myThread
 		
 thread1 = myThread(1, "Thread-1", display.lwin)
 thread2 = myThread(2, "Thread-2", display.rwin)
-
+		
 thread1.start()
 thread2.start()
 display.end(display.scr)
